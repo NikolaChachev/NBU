8afe73d0be0a61d61c2ed5d8b112f1bf914946d8
cnikola <cnikola@vmware.com>
2024-04-11 13:42:03 +0300

    Use binding in the adapter instead of old way

--- app/src/main/java/com/example/nbu/presentation/combat/CombatAdapter.java
+++ app/src/main/java/com/example/nbu/presentation/combat/CombatAdapter.java
@@ -2,24 +2,22 @@ package com.example.nbu.presentation.combat;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.nbu.R;
+import com.example.nbu.databinding.LayoutCombatLogBinding;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class CombatAdapter extends RecyclerView.Adapter<CombatAdapter.CombatViewHolder> {
 
-    private LinkedList<CombatLog> dataSet;
+    private final LinkedList<CombatLog> dataSet;
 
-    private Context context;
+    private final Context context;
 
     CombatAdapter(List<CombatLog> dataSet, Context context) {
         this.dataSet = new LinkedList<>(dataSet);
@@ -29,16 +27,15 @@ public class CombatAdapter extends RecyclerView.Adapter<CombatAdapter.CombatView
     @NonNull
     @Override
     public CombatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.layout_combat_log, parent, false);
-        return new CombatViewHolder(v);
+        LayoutInflater inf = LayoutInflater.from(parent.getContext());
+        LayoutCombatLogBinding binding = LayoutCombatLogBinding.inflate(inf, parent, false);
+        return new CombatViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CombatViewHolder holder, int position) {
         CombatLog log = dataSet.get(position);
-        holder.getTextView().setText(log.log);
-        holder.getTextView().setTextColor(ContextCompat.getColor(context, log.colorResource));
+        holder.bind(log, context);
     }
 
     @Override
@@ -52,24 +49,23 @@ public class CombatAdapter extends RecyclerView.Adapter<CombatAdapter.CombatView
     }
 
     public void insertItems(List<CombatLog> newLogs) {
-        int position = dataSet.size() - 1;
+        int position = dataSet.size();
         dataSet.addAll(newLogs);
         notifyItemRangeInserted(position, dataSet.size() - 1);
     }
 
     public static class CombatViewHolder extends RecyclerView.ViewHolder {
+        private final LayoutCombatLogBinding binding;
 
-        private final TextView textView;
-
-        public CombatViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            textView = (TextView) itemView.findViewById(R.id.combat_log_text);
-//            textView.setTextColor(Color.RED);
+        public CombatViewHolder(@NonNull LayoutCombatLogBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
 
-        public TextView getTextView() {
-            return textView;
+        public void bind(CombatLog log, Context context) {
+            binding.combatLogText.setText(log.log);
+            binding.combatLogText.setTextColor(ContextCompat.getColor(context, log.colorResource));
         }
+
     }
 }
